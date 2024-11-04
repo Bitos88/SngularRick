67338100abeeeacc4afbdbdf7493da3cd0ce4f72
Bito <tyrion72@gmail.com>
2024-11-04 11:06:25 +0100

    Initial Commit

---
+++ SngularRick/SngularRickApp.swift
@@ -0,0 +1,17 @@
+//
+//  SngularRickApp.swift
+//  SngularRick
+//
+//  Created by Alberto Alegre Bravo on 4/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SngularRickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SngularRickTests/SngularRickTests.swift
@@ -0,0 +1,17 @@
+//
+//  SngularRickTests.swift
+//  SngularRickTests
+//
+//  Created by Alberto Alegre Bravo on 4/11/24.
+//
+
+import Testing
+@testable import SngularRick
+
+struct SngularRickTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
